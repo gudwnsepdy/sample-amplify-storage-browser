b74a88b79443a3d1a6327162eb1f8ad1118ce3f0
gudwnsepdy <62203909+gudwnsepdy@users.noreply.github.com>
2025-04-09 13:25:51 +0900

    Update resource.ts
    해ㅐㅇ

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -7,6 +7,13 @@ export const storage = defineStorage({
     '*': [
         allow.guest.to(['read', 'write']),
         allow.authenticated.to(['read', 'write', 'delete']),
+    ],
+    'admin/*': [
+        allow.groups(['admin']).to(['read', 'write', 'delete']),
+        allow.authenticated.to(['read'])
+    ],
+    'private/{entity_id}/*': [
+        allow.entity('identity').to(['read', 'write', 'delete'])
     ]
    })
 });
