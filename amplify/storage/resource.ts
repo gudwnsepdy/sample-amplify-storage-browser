eb14bdc94588b49ee8f486c7d5ea6a63bc44b5bf
gudwnsepdy <62203909+gudwnsepdy@users.noreply.github.com>
2025-04-09 13:20:51 +0900

    Update resource.ts
    *

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,16 +4,9 @@ export const storage = defineStorage({
   name: 'myStorageBucket',
   isDefault: true,
    access: (allow) => ({
-    'public/*': [
+    '*': [
         allow.guest.to(['read', 'write']),
         allow.authenticated.to(['read', 'write', 'delete']),
-    ],
-    'admin/*': [
-        allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['read'])
-    ],
-    'private/{entity_id}/*': [
-        allow.entity('identity').to(['read', 'write', 'delete'])
     ]
    })
 });
